7ce69b021c9db5e958e082ef451afaa801fee124
Koteswararao <sakhamurikanna@users.noreply.github.com>
2020-02-20 19:50:34 +0530

    Update site_status.py

--- site_status.py
+++ site_status.py
@@ -8,7 +8,7 @@ import StringIO
 import csv
 
 port = 443
-default_filepath = '/Users/sakhamuri/logs1'
+default_filepath = '/var/log/stats'
 
 def isReachable(url, port):
     """
@@ -20,6 +20,7 @@ def isReachable(url, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
     try:
+       #checks site avilability
        s.connect((url, int(port)))
        s.shutdown(socket.SHUT_RDWR)
        return True
@@ -34,7 +35,7 @@ def file_exits(file):
     :argument: file path
     :return: a dirctory with reachability status
     """
-
+    #Reads the file line by line
     Read_file = open(file, 'r')
     count = 0
     results = {}
@@ -44,8 +45,10 @@ def file_exits(file):
       if not lines:
          break
       for line in lines:
+          #Extracting sites without special charecters
           url = re.findall(r'^www.[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}', line)
           str_url = str(url[0])
+          #Passing site URL to check its avilability
           url_status = isReachable(str_url, port)
           if url_status:
              results[str_url] = "100%"
@@ -57,10 +60,10 @@ def file_exits(file):
 def update_csv_file(final_result):
     """
     This function writes the result to CSV file in the given path
-    :parameter 1: takes Dictionary values  
+    :parameter 1: takes dictionary values  
     """
     try:
-        File_path = raw_input('Enter path to store results /var/log/stats(default): ')
+        File_path = raw_input('Enter path to store results '+default_filepath+' (default): ')
         if len(File_path) > 0 and os.path.exists(File_path):
            with open(os.path.join(File_path, "Sites-Availability-"+str(time.strftime('%Y%m%d%H%M%S'))+'.csv'), "w") as csvfile:
                 writer = csv.DictWriter(csvfile, fieldnames=['ADDRESS', 'STATUS'])
@@ -86,13 +89,16 @@ if __name__ == "__main__":
     :Example: python py_script2.py /path/to/urls/file.txt
     """
     try:
+         #Checks if path is passed as argument and proceeds further
          path = sys.argv[1]
          if os.path.exists(path):
             final_result = file_exits(path)
+            #Passing dictionary to write to CSV file
             update_csv_file(final_result)
          else:
             print("File doesn't exist")
 
     except IndexError:
+            #Throws error when file is not passed as argument
             print('Please provide path to URLs file!!!')
             print("USEAGE: python script.py /path/to/urls/file.txt")
